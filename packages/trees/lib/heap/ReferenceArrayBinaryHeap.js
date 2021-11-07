b1fc0285a14f62b29a2e33d27b85662d90df3b76
Meghan Dunham <mdunham@mdunham-fvfdfbrpmnhx.lan>
2021-11-07 18:01:43 -0500

    Corrections to heapUp and heapDown
    Added conditional statements to heapUp and heapDown to
    prevent error and/or infinite recursion.
    
    For heapUp check if the current index is greater than 0 to
    prevent a negative / out of bounds parent.
    
    For heapDown only attempt if there is at least a left child within
    the bounds of the current size, and only compare left and right
    children if there is a right child within the bounds of the
    current size.

--- packages/trees/lib/heap/ReferenceArrayBinaryHeap.js
+++ packages/trees/lib/heap/ReferenceArrayBinaryHeap.js
@@ -10,10 +10,10 @@
  *  It should be fairly clear to see that each node has a mathematical relationship to its
  *  children - it's left child is at 2p + 1, and it's right child is at 2p + 2.
  *  
- *  Conviently, the relationship is reversible - every node's parent is at floor((c-1)/2).
+ *  Conveniently, the relationship is reversible - every node's parent is at floor((c-1)/2).
  */
 class ReferenceArrayBinaryHeap {
-  
+
   /**
    */
   constructor(comparator) {
@@ -49,7 +49,7 @@ class ReferenceArrayBinaryHeap {
    *  number of maximum swaps:
    *  1/4 of the input has a maximum of 1 swap.
    *  1/8 of the input has a maximum of 2 swaps.
-   *  and so on, and so forth, until we get to the root node which actualy has log n maximum swaps,
+   *  and so on, and so forth, until we get to the root node which actually has log n maximum swaps,
    *  but is only a single node!  It's not hard to see from this pattern that we get an sum:
    *  1*n/4 + 2 * n/8 + 3 * n/16 + 4 * n/32... and so on and so forth.  We can factor out n and see
    *  the coefficient (1/4 + 1/4 + 3/16 + 1/8 + 5/64 + 3/64 + 7/256...) is hard to calculate, but
@@ -85,7 +85,7 @@ class ReferenceArrayBinaryHeap {
   }
 
   /**
-   *  @timeComplexity O(logn)
+   *  @timeComplexity O(log n)
    *  @spaceComplexity O(1)
    */
   extract() {
@@ -103,7 +103,7 @@ class ReferenceArrayBinaryHeap {
   isEmpty() {
     return this._size === 0;
   }
-  
+
   _calcParent(n) {
     return Math.floor((n - 1) / 2);
   }
@@ -111,7 +111,7 @@ class ReferenceArrayBinaryHeap {
   _calcLeftChild(n) {
     return 2 * n + 1
   }
-  
+
   _calcRightChild(n) {
     return 2 * n + 2
   }
@@ -121,19 +121,23 @@ class ReferenceArrayBinaryHeap {
     this._storage[a] = this._storage[b];
     this._storage[b] = tmp;
   }
-  
+
   /**
    *  Typically started at a leaf node, the heap up process filters a small element up
-   *  the heap until it statisfies the heap property.
+   *  the heap until it satisfies the heap property.
    *  
    *  @timeComplexity O(log n)
    *  @spaceComplexity O(1)
    */
   _heapUp(n) {
-    parent = this._calcParent(n);
-    if (this._comparator(this._storage[n], this._storage[parent]) < 0) {
-      this._swap(n, parent);
-      this._heapUp(parent);
+
+    // Only heap up if n greater than 0, otherwise parent will be negative / out of bounds
+    if (n > 0) {
+      parent = this._calcParent(n);
+      if (this._comparator(this._storage[n], this._storage[parent]) < 0) {
+        this._swap(n, parent);
+        this._heapUp(parent);
+      }
     }
   }
 
@@ -141,16 +145,24 @@ class ReferenceArrayBinaryHeap {
    *  This is most routinely called on the root during a lead 
    */
   _heapDown(n) {
+
     leftChild = this._calcLeftChild(n);
     rightChild = this._calcRightChild(n);
-    minChild = this._comparator(this._storage[leftChild], this._storage[rightChild]) < 0
-      ? leftChild
-      : rightChild
-    if (this._comparator(this._storage[minChild], this._storage[n]) < 0) {
-      this._swap(n, minChild);
-      this._heapUp(minChild);
-    }
+    minChild = leftChild;
 
+    // Only heap down if n has child(ren)
+    if (leftChild < this._size) {
+
+      // Check for right child and update minChild if right is less than left
+      if (rightChild < this._size && this._comparator(this._storage[rightChild], this._storage[leftChild]) < 0) {
+        minChild = rightChild;
+      }
+
+      if (this._comparator(this._storage[minChild], this._storage[n]) < 0) {
+        this._swap(n, minChild);
+        this._heapDown(minChild);
+      }
+    }
   }
 }
 
